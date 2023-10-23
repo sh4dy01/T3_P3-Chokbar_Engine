4beefa9c517996639e00b33e1e9795c894832a2d
Gotvna <nvogt@gaming.tech>
2023-10-23 14:48:14 +0200

    features(Input) : Keyboard events v0.1

--- ChokbarEngine/Source/Engine/InputHandler.cpp
+++ ChokbarEngine/Source/Engine/InputHandler.cpp
@@ -1,147 +1,28 @@
 #include "InputHandler.h"
 
-void MoveLookController::onPointerPressed(
-	_In_ Windows::UI::Core::CoreWindow^ sender,
-	_In_ Windows::UI::Core::PointerEventArgs^ args
-)
+LRESULT CALLBACK InputHandler::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	UINT32 pointerID = args->CurrentPoint->PointerId;
-	XMFLOAT2 position = XMFLOAT2(args->CurrentPoint->Position.x, args->CurrentPoint->Position.y);
-
-	auto device = args->CurrentPoint->PointerDevice;
-	auto deviceType = device->PointerDEviceType;
-	if (deviceType == PointerDeviceType::Mouse)
+	switch (uMsg)
 	{
-	}
-
-	if ((position.x < 300 && position.y > 380) && (deviceType != PointerDeviceType::Mouse))
+	case WM_KEYDOWN:
 	{
-		if (!m_moveInUse)
+		switch (wParam)
 		{
-			m_moveFirstDown = position;
-			m_movePointerPosition = position;
-			m_movePointerID = pointerID;
-			m_moveInUse = true;
-		}
-	}
-	else
-	{
-		if (!m_lookInUse)
-		{
-			m_lookLastPoint = position;
-			m_lookPointerID = args->CurrentPoint->PointerId;
-			m_lookLastDelta.x = 0;
-			m_lookLastDelta.y = 0;
-			m_lookInUse = true;
+		case 'A':
+			MessageBox(NULL, L"A", L"Key Pressed", MB_OK);
+			break;
+		case 'D':
+			MessageBox(NULL, L"D", L"Key Pressed", MB_OK);
+			break;
+		case VK_SHIFT :
+			MessageBox(NULL, L"Shift", L"Key Pressed", MB_OK); //Bonus
+			break;
+		case VK_INSERT:
+			MessageBox(NULL, L"Insert", L"Key Pressed", MB_OK); //Change Sound
+			break;
 		}
 	}
-}
+	break;
 
-void MoveLookController::onPointerMoved(
-_In_ Windows::UI::Core::CoreWindow^ sender,
-_In_ Windows::UI::Core::PointerEventArgs^ args
-)
-{
-	UINT32 pointerID = args->CurrentPoint->PointerId;
-	XMFLOAT2 position = XMFLOAT2(args->CurrentPoint->Position.x, args->CurrentPoint->Position.y);
-
-	if (pointerID == m_movePointerID)
-	{
-		m_movePointerPosition = position;
-	}
-	else if (pointerID == m_lookPointerID)
-	{
-		
-		m_delta.x = position.x - m_lookLastPoint.x;
-		m_delta.y = position.y - m_lookLastPoint.y;
-
-		m_RotationDelta.x = m_delta.x * ROTATION_GAIN;
-		m_RotationDelta.y = m_delta.y * ROTATION_GAIN;
-		m_lookLastPoint = position;
-
-		m_pitch -= m_RotationDelta.y;
-		m_yaw -= m_RotationDelta.x;
-
-		m_pitch = (float)__max(-XM_PI / 2.0f, m_pitch);
-		m_pitch = (float)__min(+XM_PI / 2.0f, m_pitch);
-
-	}
-}
-
-void MoveLookController::onPointerReleased(
-	_In_ Windows::UI::Core::CoreWindow^ sender,
-	_In_ Windows::UI::Core::PointerEventArgs^ args
-)
-{
-	UINT32 pointerID = args->CurrentPoint->PointerId;
-	XMFLOAT2 position = XMFLOAT2(args->CurrentPoint->Position.x, args->CurrentPoint->Position.y);
-
-	if (pointerID == m_movePointerID)
-	{
-		m_moveInUse = false;
-		m_movePointerID = 0;
-	}
-	else if (pointerID == m_lookPointerID)
-	{
-		m_lookInUse = false;
-		m_lookPointerID = 0;
-	}
-}
-
-void MoveLookController::onKeyDown(
-	_In_ Windows::UI::Core::CoreWindow^ sender,
-	_In_ Windows::UI::Core::PointerEventArgs^ args
-)
-{
-	Windows::System::VirtualKey Key;
-	Key = args->VirtualKey;
-
-	switch (Key)
-	{
-		if (Key == VirtualKey::Z)
-		{
-			m_forward = true;
-		}
-		if (Key == VirtualKey::S)
-		{
-			m_back = true;
-		}
-		if (Key == VirtualKey::Q)
-		{
-			m_left = true;
-		}
-		if (Key == VirtualKey::D)
-		{
-			m_right = true;
-		}
-	}
-}
-
-void MoveLookController::onKeyUp(
-	_In_ Windows::UI::Core::CoreWindow^ sender,
-	_In_ Windows::UI::Core::PointerEventArgs^ args
-)
-{
-	Windows::System::VirtualKey Key;
-	Key = args->VirtualKey;
-
-	switch (Key)
-	{
-		if (Key == VirtualKey::Z)
-		{
-			m_forward = false;
-		}
-		if (Key == VirtualKey::S)
-		{
-			m_back = false;
-		}
-		if (Key == VirtualKey::Q)
-		{
-			m_left = false;
-		}
-		if (Key == VirtualKey::D)
-		{
-			m_right = false;
-		}
-	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
--- ChokbarEngine/Source/Engine/InputHandler.h
+++ ChokbarEngine/Source/Engine/InputHandler.h
@@ -1,68 +1,13 @@
+#include <wchar.h>
+#include <WinUser.h>
+#include <wtypes.h>
 
-#define ROTATION_GAIN 0.004f
-#define MOVEMENT_GAIN 0.1f
-
-#include <DirectXMath.h>
-#include "Chokbar.h"
-using namespace DirectX;
-
-class MoveLookController
+class InputHandler
 {
-private: 
-	XMFLOAT3 m_position;
-	float m_pitch;
-	float m_yaw;
-
-	bool m_forward;
-	bool m_back;
-	bool m_left;
-	bool m_right;
-	bool m_up;
-	bool m_down;
-
-	bool m_moveInUse;
-	UINT32 m_movePointerID;
-	XMFLOAT2 m_moveFirstDown;
-	XMFLOAT2 m_movePointerPosition;
-	XMFLOAT2 m_moveCommand;
-
-	bool m_lookInUse;
-	UINT32 m_lookPointerID;
-	XMFLOAT2 m_lookLastPoint;
-	XMFLOAT2 m_lookLastDelta;
-	XMFLOAT2 m_delta;
-	XMFLOAT2 m_RotationDelta;
-
 public: 
-	void onPointerPressed(
-		_In_ Windows::UI::Core::CoreWindow^ sender, 
-		_In_ Windows::UI::Core::PointerEventArgs^ args
-		);
-	void onPointerMoved(
-		_In_ Windows::UI::Core::CoreWindow^ sender,
-		_In_ Windows::UI::Core::PointerEventArgs^ args
-		);
-	void onPointerReleased(
-		_In_ Windows::UI::Core::CoreWindow^ sender,
-		_In_ Windows::UI::Core::PointerEventArgs^ args
-		);
-	void onKeyDown(
-		_In_ Windows::UI::Core::CoreWindow^ sender,
-		_In_ Windows::UI::Core::KeyEventArgs^ args
-		);
-	void onKeyUp(
-		_In_ Windows::UI::Core::CoreWindow^ sender,
-		_In_ Windows::UI::Core::KeyEventArgs^ args
-		);
 
-	void Initialize(_In_ Windows::UI::Core::CoreWindow^ window);
+	LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-	void Update(Windows::UI::Core::CoreWindow^ window);
-
-	void setPosition(_In_ XMFLOAT3 pos);
-	void setOrientation(_In_ float pitch, _In_ float yaw);
-
-	XMFLOAT3 getPosition();
-	XMFLOAT2 getLookPoint();
+	// Thats all
 
 };
