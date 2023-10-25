bbf0456686921f2c5a80882f8c7c39c0e792f4be
Hugo.M <h.maestracci83@gmail.com>
2023-10-26 01:54:17 +0200

    feature: added memory leak detection

--- ChokbarEngine/Source/Platform/Windows/WinEntry.h
+++ ChokbarEngine/Source/Platform/Windows/WinEntry.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define DBG_NEW new
+#endif
+
 #include "IApplication.h"
 #include "Core/DebugUtils.h"
 
@@ -30,7 +42,8 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
 		return 0;
 	}
-	
+
+	_CrtDumpMemoryLeaks();
 
 	return 0;
 }
