a6d5ab9a18236816e8632a783253a9db148ba82d
Hugo M <h.maestracci83@gmail.com>
2023-10-27 14:35:21 +0200

    chore: remove class to pch

--- ChokbarEngine/Source/Core/Core.h
+++ ChokbarEngine/Source/Core/Core.h
@@ -5,9 +5,6 @@
 
 #include "CoreMinimal.h"
 
-#include "Engine/ECS/Base/Coordinator.h"
-#include "Engine/ECS/Systems/TransformSystem.h"
-
 #include "Engine/Engine.h"
 #include "Engine/GameTimer.h"
 
