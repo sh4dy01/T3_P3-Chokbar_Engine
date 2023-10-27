0ebe267feeb72c7231034c512d726aa4c0e101ab
Hugo.M <h.maestracci83@gmail.com>
2023-10-28 01:44:03 +0200

    chore: fix lost bracket

--- ChokbarEngine/Source/Engine/ECS/Components/Component.h
+++ ChokbarEngine/Source/Engine/ECS/Components/Component.h
@@ -3,16 +3,15 @@
 class GameObject;
 class Transform;
 
-	class Component
-	{
-	public:
-		GameObject* gameObject;
-		Transform* transform;
+class Component
+{
+public:
+	GameObject* gameObject;
+	Transform* transform;
 
-		void SetEnabled(bool enabled) { m_isEnabled = enabled; }
-		bool IsEnabled() const { return m_isEnabled; }
+	void SetEnabled(bool enabled) { m_isEnabled = enabled; }
+	bool IsEnabled() const { return m_isEnabled; }
 
-	private:
-		bool m_isEnabled = false;
-	};
-}
+private:
+	bool m_isEnabled = false;
+};
