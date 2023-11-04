8f212ee18679bb49bef56b2006b8c24dc6013c88
Hugo.M <h.maestracci83@gmail.com>
2023-11-04 20:00:34 +0100

    chore: edit to show the slow motion

--- BlankProject/Source/Scripts/PlayerComponent.cpp
+++ BlankProject/Source/Scripts/PlayerComponent.cpp
@@ -15,38 +15,40 @@ void PlayerComponent::Update()
 {
 	if (InputHandler::IsKeyHeld('z'))
 	{
-		transform->Translate(0, 0, m_Speed * GameTimer::GetDeltaTime());
+		transform->Translate(0, 0, m_Speed * TimeManager::GetDeltaTime());
 	}
 	if (InputHandler::IsKeyHeld('s'))
 	{
-		transform->Translate(0, 0, -m_Speed * GameTimer::GetDeltaTime());
+		transform->Translate(0, 0, -m_Speed * TimeManager::GetDeltaTime());
 	}
 	if (InputHandler::IsKeyHeld('q'))
 	{
-		transform->Translate(-m_Speed * GameTimer::GetDeltaTime(), 0, 0);
+		transform->Translate(-m_Speed * TimeManager::GetDeltaTime(), 0, 0);
 	}
 	if (InputHandler::IsKeyHeld('d'))
 	{
-		transform->Translate(m_Speed * GameTimer::GetDeltaTime(), 0, 0);
+		transform->Translate(m_Speed * TimeManager::GetDeltaTime(), 0, 0);
 	}
 	if (InputHandler::IsKeyDown(VK_RBUTTON))
 	{
-		m_pCamera->SetFOV(10.f);
+		//m_pCamera->SetFOV(10.f);
+		TimeManager::SetTimeScale(0.1f);
 	}
 	else if (InputHandler::IsKeyUp(VK_RBUTTON))
 	{
-		m_pCamera->SetFOV(75.f);
+		//m_pCamera->SetFOV(75.f);
+		TimeManager::SetTimeScale(1.f);
 	}
 	if (InputHandler::IsKeyHeld(VK_SHIFT))
 	{
-		transform->Translate(0, -m_Speed * GameTimer::GetDeltaTime(), 0, Transform::Space::World);
+		transform->Translate(0, -m_Speed * TimeManager::GetDeltaTime(), 0, Transform::Space::World);
 	}
 	if (InputHandler::IsKeyHeld(VK_SPACE))
 	{
-		transform->Translate(0, m_Speed * GameTimer::GetDeltaTime(), 0, Transform::Space::World);
+		transform->Translate(0, m_Speed * TimeManager::GetDeltaTime(), 0, Transform::Space::World);
 	}
 
-	transform->RotateYaw(InputHandler::GetAxisX() * 50.f * GameTimer::GetDeltaTime(), Transform::Space::World);
+	transform->RotateYaw(InputHandler::GetAxisX() * 50.f * TimeManager::GetDeltaTime(), Transform::Space::World);
 
-	transform->RotatePitch(InputHandler::GetAxisY() * 50.f * GameTimer::GetDeltaTime());
+	transform->RotatePitch(InputHandler::GetAxisY() * 50.f * TimeManager::GetDeltaTime());
 }
